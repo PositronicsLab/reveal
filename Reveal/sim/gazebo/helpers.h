f48610324bcd324b8677d92e5b3471ff04d90a15
James <james@PR2Lab-2.(none)>
2015-05-29 11:40:28 -0400

    documented sim/gazebo

--- Reveal/sim/gazebo/helpers.h
+++ Reveal/sim/gazebo/helpers.h
@@ -20,56 +20,116 @@ namespace Gazebo {
 
 class helpers_c {
 public:
-  // write a reveal model into gazebo to update the simulator
+  /// writes a reveal model into gazebo to update the simulator
+  /// @param model the reveal model to write into gazebo
+  /// @param world the gazebo world to update
+  /// @return true if the operation succeeds OR false if the operation fails
+  ///         for any reason
   static bool write_model( Reveal::Core::model_ptr model, gazebo::physics::WorldPtr world );
 
-  // write a reveal trial into gazebo to update the simulator
+  /// writes a reveal trial into gazebo to update the simulator
+  /// @param trial the reveal trial to write into gazebo
+  /// @param experiment the experimental parameters to write into gazebo
+  /// @param world the gazebo world to update
+  /// @return true if the operation succeeds OR false if the operation fails
+  ///         for any reason
   static bool write_trial( Reveal::Core::trial_ptr trial, Reveal::Core::experiment_ptr experiment, gazebo::physics::WorldPtr world );
 
-  // read out a gazebo state as a gazebo trial
-
-  // read out a gazebo model as a reveal model
+  /// reads out a gazebo model as a reveal model
+  /// @param world the gazebo world to read from
+  /// @param model_name the name of the model to read
+  /// @param control the map of the controls assigned to the model
+  /// @return the reveal model produced by reading from gazebo
   static Reveal::Core::model_ptr read_model( gazebo::physics::WorldPtr world, std::string model_name, std::map<std::string,double> control );
 
-  // read out a gazebo state as a reveal model solution
+  /// reads out a gazebo state as a reveal model solution
+  /// @param world the gazebo world to read from
+  /// @param model_list the list of all the models to read into the solution
+  /// @param scenario_id the scenario identifier
+  /// @return the reveal model solution produced by reading from gazebo
   static Reveal::Core::solution_ptr read_model_solution( gazebo::physics::WorldPtr world, std::vector<std::string> model_list, std::string scenario_id );
 
-  // read out a gazebo state as a reveal client solution
+  /// read out a gazebo state as a reveal client solution
+  /// @param world the gazebo world to read from
+  /// @param model_list the list of all the models to read into the solution
+  /// @param scenario_id the scenario identifier
+  /// @return the reveal client solution produced by reading from gazebo
   static Reveal::Core::solution_ptr read_client_solution( gazebo::physics::WorldPtr world, std::vector<std::string> model_list, std::string scenario_id );
 
-  // map a reveal state's angular velocity into a gazebo vector
+  /// maps a reveal state's angular velocity into a gazebo vector
+  /// @param state the reveal state to map from
+  /// @return the gazebo compatible position vector produced from state data
   static gazebo::math::Vector3 position( const Reveal::Core::state_c& state );
-  // map a reveal state's rotation into a gazebo quaternion
+
+  /// maps a reveal state's rotation into a gazebo quaternion
+  /// @param state the reveal state to map from
+  /// @return the gazebo compatible rotation quaternion produced from state data
   static gazebo::math::Quaternion rotation( const Reveal::Core::state_c& state );
-  // map a reveal state's linear velocity into a gazebo vector
+  /// maps a reveal state's linear velocity into a gazebo vector
+  /// @param state the reveal state to map from
+  /// @return the gazebo compatible linear velocity vector produced from state 
+  ///         data
   static gazebo::math::Vector3 linear_velocity( const Reveal::Core::state_c& state );
-  // map a reveal state's angular velocity into a gazebo vector
+
+  /// maps a reveal state's angular velocity into a gazebo vector
+  /// @param state the reveal state to map from
+  /// @return the gazebo compatible angular velocity vector produced from state 
+  ///         data
   static gazebo::math::Vector3 angular_velocity( const Reveal::Core::state_c& state );
-  // map a reveal state into a gazebo pose structure
+
+  /// maps a reveal state into a gazebo pose structure
+  /// @param state the reveal state to map from
+  /// @return the gazebo compatible pose instance produced from state data
   static gazebo::math::Pose pose( const Reveal::Core::state_c& state );
 
-  // map a gazebo position into a reveal state
+  /// maps a gazebo position into a reveal state
+  /// @param pos the gazebo position vector to map from
+  /// @param state the reveal state to map position into
   static void position( const gazebo::math::Vector3 pos, Reveal::Core::state_c& state );
-  // map a gazebo rotation into a reveal state
+
+  /// maps a gazebo rotation into a reveal state
+  /// @param rot the gazebo rotation quaternion to map from
+  /// @param state the reveal state to map rotation into
   static void rotation( const gazebo::math::Quaternion rot, Reveal::Core::state_c& state );
-  // map a gazebo linear velocity into a reveal state
+
+  /// maps a gazebo linear velocity into a reveal state
+  /// @param lvel the gazebo linear velocity vector to map from
+  /// @param state the reveal state to map linear velocity into
   static void linear_velocity( const gazebo::math::Vector3 lvel, Reveal::Core::state_c& state );
-  // map a gazebo angular velocity into a reveal state
+
+  /// map a gazebo angular velocity into a reveal state
+  /// @param avel the gazebo angular velocity vector to map from
+  /// @param state the reveal state to map angular velocity into
   static void angular_velocity( const gazebo::math::Vector3 avel, Reveal::Core::state_c& state );
-  // map a gazebo pose into a reveal state  
+
+  /// maps a gazebo pose into a reveal state  
+  /// @param pose the gazebo pose instance to map from
+  /// @param state the reveal state to map pose into
   static void pose( const gazebo::math::Pose pose, Reveal::Core::state_c& state );
 
-  // get a gazebo world's simulation time
+  /// gets a gazebo world's simulation time
+  /// @param world the gazebo world to read virtual time from
+  /// @return the virtual time in seconds as reported by gazebo
   static double sim_time( gazebo::physics::WorldPtr world );
-  // set a gazebo world's simulation time
-  static void sim_time( double t, gazebo::physics::WorldPtr world );
-  // get a gazebo world's step size
-  static double step_size( gazebo::physics::WorldPtr world );
-  // get a gazebo world's real time
-  static double real_time( gazebo::physics::WorldPtr world );
-  // reset a gazebo world
-  static void reset( gazebo::physics::WorldPtr world );
 
+  /// sets a gazebo world's simulation time
+  /// @param t the virtual time in seconds to map into gazebo 
+  /// @param world the gazebo world to set the virtual time of
+  static void sim_time( double t, gazebo::physics::WorldPtr world );
+
+  /// gets a gazebo world's step size
+  /// @param world the gazebo world to read the integration step size from
+  /// @return the integration step size in seconds as reported by gazebo
+  static double step_size( gazebo::physics::WorldPtr world );
+
+  /// gets a gazebo world's real time
+  /// @param world the gazebo world to read the real time progress from
+  /// @return the real time progress in seconds as reported by gazebo
+  static double real_time( gazebo::physics::WorldPtr world );
+
+  /// resets a gazebo world
+  /// @param world the gazebo world to reset
+  static void reset( gazebo::physics::WorldPtr world );
 };
 
 //-----------------------------------------------------------------------------
